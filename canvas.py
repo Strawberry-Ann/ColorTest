3400032b6bb4ac87cf24f66edb75e15d60de5567
Strawberry-Ann <maulann@yandex.ru>
2021-03-22 11:03:49 +0500

    Добавлено обновление файлов в папке user при каждом запуске программы, добавлена палитра и выбор цвета

--- canvas.py
+++ canvas.py
@@ -5,6 +5,8 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 import sys
 from PIL import Image
+import os
+import shutil
 
 
 class MyWidget(QMainWindow):
@@ -14,23 +16,34 @@ class MyWidget(QMainWindow):
         # Обратите внимание: имя элемента такое же как в QTDesigner
         self.x = 4
         self.name = 1
-        self.run()
-        self.color = (255, 255, 155)
+        self.initUI()
+        self.copy_files()
+        self.run_picture()
+        self.color = (255, 255, 255)
 
-    def run(self):
+    def initUI(self):
+        self.bg.buttonClicked.connect(self.check_color)
+
+    def copy_files(self):
+        lst_of_files_in_user = os.listdir(path="user")
+        for file in lst_of_files_in_user:
+            os.remove(f'user/{file}')
+        lst_of_files_in_data = list(filter(lambda x: '.bmp' == x[-4:], os.listdir(path="data")))
+        for file in lst_of_files_in_data:
+            shutil.copyfile(f'data/{file}', f'user/{file}')
+
+    def run_picture(self):
         self.look_picture(self.name)
-        # self.im = Image.open(f'data/{self.name}.jpg')
-        # self.pixels = self.im.load()  # список с пикселями
-        # self.x, self.y = self.im.size  # ширина (x) и высота (y) изображения
-        # for i in range(self.x):
-        #     for j in range(self.y):
-        #         if self.pixels[i, j] != (0, 0, 0):
-        #             self.pixels[i, j] = (255, 255, 255)
-        # self.im.save(f'user/{self.name}.jpg')
-        # Имя элемента совпадает с objectName в QTDesigner
+
+    def check_color(self, btn):
+        color = btn.palette().button().color()
+        r = color.red()
+        g = color.green()
+        b = color.blue()
+        self.color = (r, g, b)
 
     def mousePressEvent(self, event):
-        self.coords = event.x() - 9, event.y()
+        self.coords = event.x() - 9, event.y() - 9
         if 763 >= event.x() >= 9 and 541 >= event.y() >= 9:
             self.im = Image.open(f'user/{self.name}.bmp')
             self.pixels = self.im.load()  # список с пикселями
@@ -64,6 +77,10 @@ class MyWidget(QMainWindow):
                     stack.add((x, y - 1))
                 if y < (ysize - 1):
                     stack.add((x, y + 1))
+
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Escape:
+            self.close()
 
 
 if __name__ == '__main__':
